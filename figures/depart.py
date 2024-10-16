d5d23471d4205684079cfe2bb9574f99b5c3b1af
hjx <none>
2024-10-17 00:01:53 +0800

    lmm3.10

--- figures/depart.py
+++ figures/depart.py
@@ -33,20 +33,20 @@ class Depart(Scene):
         labelkp1 = MathTex(r"k+1", color=RED).move_to(I_3_triangle).shift(DOWN*2).scale(0.5)
         I_3_dots = MathTex(r"\cdots", color=RED).move_to(I_3_triangle_sk).shift(RIGHT)
         I_3_triangle_sk_2i_1 = I_3_triangle_sk.copy().move_to(I_3_dots).shift(RIGHT)
-        label2i_1 = MathTex(r"2i-1", color=RED).move_to(I_3_triangle_sk_2i_1).shift(DOWN*2).scale(0.5)
+        label2i_1 = MathTex(r"m-1", color=RED).move_to(I_3_triangle_sk_2i_1).shift(DOWN*2).scale(0.5)
         labelI_3 = MathTex(r"I_3", color=RED).move_to(I_3_dots).shift(UP*2)
 
         I_3 = VGroup(I_3_triangle_sk, labelkp1, I_3_dots, I_3_triangle_sk_2i_1, label2i_1, labelI_3)
 
         I_4_circle = Circle(radius=1, color=GREEN_D, fill_color=GREEN_D, fill_opacity=1).scale(0.3)
         I_4_circle_tri = VGroup(I_4_circle, I_4_circle.copy().shift(DOWN), I_4_circle.copy().shift(DL))
-        label2i = MathTex(r"2i", color=GREEN_D).move_to(I_4_circle).shift(DOWN*2).scale(0.5)
+        label2i = MathTex(r"m", color=GREEN_D).move_to(I_4_circle).shift(DOWN*2).scale(0.5)
         labelI_4 = MathTex(r"I_4", color=GREEN_D).move_to(I_4_circle).shift(UP*2).shift(LEFT*0.5)
         I_4 = VGroup(I_4_circle_tri, label2i, labelI_4)
 
         I_5_square = Square(side_length=2, fill_color=BLUE_D, fill_opacity=1).scale(0.3)
         I_5_sqr_col = VGroup(I_5_square, I_5_square.copy().shift(UP), I_5_square.copy().shift(DOWN))
-        label2ip1 = MathTex(r"2i+1", color=BLUE_D).move_to(I_5_sqr_col).shift(DOWN*2).scale(0.5)
+        label2ip1 = MathTex(r"m+1", color=BLUE_D).move_to(I_5_sqr_col).shift(DOWN*2).scale(0.5)
         I_5_dots = MathTex(r"\cdots", color=BLUE_D).move_to(I_5_sqr_col).shift(RIGHT)
         I_5_sqr_col_2i_1 = I_5_sqr_col.copy().move_to(I_5_dots).shift(RIGHT)
         label2N = MathTex(r"2N", color=BLUE_D).move_to(I_5_sqr_col_2i_1).shift(DOWN*2).scale(0.5)
